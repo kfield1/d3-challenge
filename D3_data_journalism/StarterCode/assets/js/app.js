be54f01928f4d2f767b40746eb180e49b723b915
kfield1 <kayt1303@yahoo.com>
2021-06-21 10:08:31 -0500

    working on d3 app

---
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -0,0 +1,73 @@
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#plot")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("/data.csv").then(d => {
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([4, 8])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 9])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+ // Highlight the specie that is hovered
+ var highlight = function(d){
+
+    //selected_specie = d.Species
+
+    d3.selectAll(".dot")
+      .transition()
+      .duration(200)
+      .style("fill", "lightgrey")
+      .attr("r", 3)
+
+    d3.selectAll("." + selected_state)
+      .transition()
+      .duration(200)
+      .style("fill", color(selected_state))
+      .attr("r", 7)
+  }
+
+  // Highlight the specie that is hovered
+  var doNotHighlight = function(){
+    d3.selectAll(".dot")
+      .transition()
+      .duration(200)
+      .style("fill", "lightgrey")
+      .attr("r", 5 )
+  }
+
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data("data.csv")
+    .enter()
+    .append("circle")
+      //.attr("class", function (d) { return "dot " + d. } )
+      .attr("cx", function (d) { return x(d.poverty); } )
+      .attr("cy", function (d) { return y(d.healthcare); } )
+      .attr("r", 5)
+      //.style("fill", function (d) { return color(d.Species) } )
+    .on("mouseover", highlight)
+    .on("mouseleave", doNotHighlight )
+
+})
